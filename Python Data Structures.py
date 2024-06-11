f80ded9fce258d734ee5a4b40b25dc0f6395ecc4
Nithish <nithishbharadwaj.akula@gmail.com>
2024-06-10 23:51:39 -0400

    proceed

---
+++ Python Data Structures.py
@@ -0,0 +1,22 @@
+#How Algorithm Works: Algorithm is a set of instructions to solve a problem.
+#Defining or adding two numbers
+num1 = 2
+num2 = 7
+sum = num1 + num2
+print(sum)
+
+#Finding largest numbers among the three
+
+a=2;b=3;c=7;
+print(a,b,c)
+if a > b:
+    if a > c:
+        print("a is the largest")
+    else:
+        print("c is the largest")
+else:
+    if b > c:
+        print("b is the largest")
+    else:
+        print('c is the largest')
+#
