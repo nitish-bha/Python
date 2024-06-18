95a39019260cbbaff2e076b6519a2ec4233c2aa2
Nithish <nithishbharadwaj.akula@gmail.com>
2024-06-17 22:43:21 -0400

    proceed

--- Binary search.py
+++ Binary search.py
@@ -18,3 +18,28 @@ if result == -1:
     print("Element is not found in the array")
 else:
     print(f"Element found at the index : {result}")
+
+
+#recursive
+
+array = [3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print(array)
+
+x = 30
+
+def binarysearch(array,x,low,high):
+    if high >= low:
+        mid = low + (high-low)//2
+
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            return binarysearch(array,x,mid+1,high)
+        else:
+            return binarysearch(array,x,low,mid-1)
+    return -1
+result = binarysearch(array,x,0,len(array)-1)
+if result == -1:
+    print("Element is not found in the array")
+else:
+    print(f"Element found at the index : {result}")
